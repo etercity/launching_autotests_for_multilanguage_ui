398d6859cb5b2470f4e7ca23a23093a447b27105
Alex QA <etercity@gmail.com>
2021-08-13 15:53:15 +0300

    one minor comment add

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_button_add_exist(browser):
     browser.implicitly_wait(10)
     browser.get(link)
-    # time.sleep(10)
+    # time.sleep(10) #Раскоментить для визуальной проверки
     button = browser.find_element_by_css_selector('.btn-add-to-basket')
     assert button, "button is missing"
 
